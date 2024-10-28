cbf18e7d3a6b6ad08539ff89a0c37e10b5636246
Valentina Galeano Orozco <valengaleano04@gmail.com>
2024-10-28 11:55:36 -0500

    Add files via upload

---
+++ frontedcliente_surag5/CLIENTE_SURATECH_G5-main/src/controllers/controladorsignovital.js
@@ -0,0 +1,3 @@
+let nombreSignovital=document.getElementById("nombresignovital")
+let valorSignovital=document.getElementById("valorsignovital")
+let fechaMedidaSignovital=document.getElementById("fechamedidasignovital")
